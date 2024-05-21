305cdf181694d01ae546d505b1cd141601fce8a4
Zidna <zidnasoledazulfa@gmail.com>
2024-05-22 00:55:35 +0700

    First init Github

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('My Profile')
+st.subheader('Nama : Zidna Soleda Zulfa')
+st.subheader('NIM : 312210031')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Informatika')
+
+st.image("./img/zidna.jpg", caption="Zidna", use_column_width="always", width=200)
+
+st.link_button("Instagram", "https://www.instagram.com/zidna_zee?igsh=MWxwN3I4ZjVrMXl1dw==")
+st.link_button("Github", "https://github.com/Zidna090702")
